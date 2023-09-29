bc90bb0768cfb9040786bcf311eea86b76e735ac
tinb2014796 <tinb2014796@student.ctu.edu.vn>
2023-09-29 15:39:32 +0700

    update repo

---
+++ For.java
@@ -0,0 +1,8 @@
+package CT3002.CT3002;
+public class For{
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+          }
+    }
+}
